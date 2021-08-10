2ccb7e21526cc357cf7ea77bfa487d8b4b8ec370
ivanfsilva <ivanfs27@gmail.com>
2021-08-09 21:53:11 -0300

    using Lombok

--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Cozinha.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Cozinha.java
@@ -1,33 +1,23 @@
 package br.com.ivanfsilva.webfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "cozinha")
 public class Cozinha {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 30)
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     @Override
     public boolean equals(Object o) {
