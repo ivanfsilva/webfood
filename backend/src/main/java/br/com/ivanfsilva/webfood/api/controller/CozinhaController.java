bbc59d48c8fd52266dce0364449bdbe8a6cbe0a5
ivanfsilva <ivanfs27@gmail.com>
2021-08-14 20:42:21 -0300

    Implementing content negotiation to return JSON or XML

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.com.ivanfsilva.webfood.api.controller;
 import br.com.ivanfsilva.webfood.domain.model.Cozinha;
 import br.com.ivanfsilva.webfood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
