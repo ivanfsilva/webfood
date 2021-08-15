9dd02e6196c235263675bd7c3e4b7c644614c27d
ivanfsilva <ivanfs27@gmail.com>
2021-08-15 20:14:21 -0300

    Implementing Kitchen Update with PUT

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.com.ivanfsilva.webfood.api.controller;
 import br.com.ivanfsilva.webfood.api.model.CozinhasXmlWrapper;
 import br.com.ivanfsilva.webfood.domain.model.Cozinha;
 import br.com.ivanfsilva.webfood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -43,5 +44,20 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar( @RequestBody Cozinha cozinha ) {
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+                                             @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+            cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
