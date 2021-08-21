25abcec7e2d98b651d491be7b54bae9f76a45fa7
ivanfsilva <ivanfs27@gmail.com>
2021-08-21 13:00:46 -0300

    refactoring all repositories to use SDJ

--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/repository/EstadoRepository.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/repository/EstadoRepository.java
@@ -1,14 +1,9 @@
 package br.com.ivanfsilva.webfood.domain.repository;
 
 import br.com.ivanfsilva.webfood.domain.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface EstadoRepository {
-
-    List<Estado> listar();
-    Estado buscar(Long id);
-    Estado salvar(Estado estado);
-    void remover(Long id);
-
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
 }
--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/repository/RestauranteRepository.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/repository/RestauranteRepository.java
@@ -1,14 +1,9 @@
 package br.com.ivanfsilva.webfood.domain.repository;
 
 import br.com.ivanfsilva.webfood.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface RestauranteRepository {
-
-    List<Restaurante> listar();
-    Restaurante buscar(Long id);
-    Restaurante salvar(Restaurante restaurante);
-    void remover(Restaurante restaurante);
-
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 }
