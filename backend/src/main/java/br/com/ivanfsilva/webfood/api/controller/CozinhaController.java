d5f427440c80a3cdbeae8fb2467531b9e16fb49c
ivanfsilva <ivanfs27@gmail.com>
2021-08-24 21:49:50 -0300

    Throwing ResponseStatusException type exceptions

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CozinhaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -83,17 +84,28 @@ public class CozinhaController {
         return ResponseEntity.notFound().build();
     }
 
+//    @DeleteMapping("/{cozinhaId}")
+//    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+//        try {
+//            cadastroCozinha.excluir(cozinhaId);
+//            return ResponseEntity.noContent().build();
+//
+//        } catch (EntidadeNaoEncontradaException e) {
+//            return ResponseEntity.notFound().build();
+//
+//        } catch (EntidadeEmUsoException e) {
+//            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+//        }
+//    }
+
     @DeleteMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long cozinhaId) {
         try {
             cadastroCozinha.excluir(cozinhaId);
-            return ResponseEntity.noContent().build();
-
         } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.notFound().build();
-
-        } catch (EntidadeEmUsoException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+//			throw new ServerWebInputException(e.getMessage());
         }
     }
 }
--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/exception/EntidadeEmUsoException.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/exception/EntidadeEmUsoException.java
@@ -3,7 +3,7 @@ package br.com.ivanfsilva.webfood.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada")
+@ResponseStatus(HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
