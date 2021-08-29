2f87aff715eb471633e957190227ce7236394e61
ivanfsilva <ivanfs27@gmail.com>
2021-08-29 12:18:31 -0300

    handling invalid URL parameter exception

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/exceptionhandler/ApiExceptionHandler.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +20,48 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // 1. MethodArgumentTypeMismatchException é um subtipo de TypeMismatchException
+
+    // 2. ResponseEntityExceptionHandler já trata TypeMismatchException de forma mais abrangente
+
+    // 3. Então, especializamos o método handleTypeMismatch e verificamos se a exception
+    //    é uma instância de MethodArgumentTypeMismatchException
+
+    // 4. Se for, chamamos um método especialista em tratar esse tipo de exception
+
+    // 5. Poderíamos fazer tudo dentro de handleTypeMismatch, mas preferi separar em outro método
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+                                                        HttpStatus status, WebRequest request) {
+
+        if (ex instanceof MethodArgumentTypeMismatchException) {
+            return handleMethodArgumentTypeMismatch(
+                    (MethodArgumentTypeMismatchException) ex, headers, status, request);
+        }
+
+        return super.handleTypeMismatch(ex, headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+            MethodArgumentTypeMismatchException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.PARAMETRO_INVALIDO;
+
+        String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', "
+                        + "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+                ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
