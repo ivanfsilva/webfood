cfa6d0083acf031c203a5875813c63e684eb3a13
ivanfsilva <ivanfs27@gmail.com>
2021-08-29 18:31:01 -0300

    Validating the Associations of a Cascading Entity

--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -35,6 +36,8 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
+    @Valid
+    @NotNull
     @JsonIgnoreProperties("hibernateLazyInitializer")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cozinha_id", nullable = false)
