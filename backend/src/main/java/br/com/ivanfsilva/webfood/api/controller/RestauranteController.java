7ac1915d501e1c327d477393d731aa598884b20f
ivanfsilva <ivanfs27@gmail.com>
2021-08-29 17:30:35 -0300

    Adding constraints and validating the controller with @Valid

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/RestauranteController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/RestauranteController.java
@@ -18,6 +18,7 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -84,7 +85,7 @@ public class RestauranteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+    public Restaurante adicionar(@Valid @RequestBody Restaurante restaurante) {
         try {
             return cadastroRestaurante.salvar(restaurante);
         } catch (CozinhaNaoEncontradaException e) {
