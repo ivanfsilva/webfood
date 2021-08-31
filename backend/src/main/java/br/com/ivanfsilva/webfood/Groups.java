77ff14f63b5cf9afb1f48633a63952ea952ad4c0
ivanfsilva <ivanfs27@gmail.com>
2021-08-30 21:55:08 -0300

    adding validation constraints to the model

--- backend/src/main/java/br/com/ivanfsilva/webfood/Groups.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/Groups.java
@@ -2,4 +2,6 @@ package br.com.ivanfsilva.webfood;
 
 public interface Groups {
     public interface CozinhaId {}
+
+    public interface EstadoId {}
 }
--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Estado.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Estado.java
@@ -1,20 +1,25 @@
 package br.com.ivanfsilva.webfood.domain.model;
 
+import br.com.ivanfsilva.webfood.Groups;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Estado {
 
+    @NotNull(groups = Groups.EstadoId.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
