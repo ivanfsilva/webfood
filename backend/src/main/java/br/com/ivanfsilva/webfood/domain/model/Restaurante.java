b4a665dd48e6ab64f04e1be38697d302e3dfb43b
ivanfsilva <ivanfs27@gmail.com>
2021-08-31 12:43:15 -0300

    Creating custom validation constraints using composition

--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.webfood.domain.model;
 
 import br.com.ivanfsilva.webfood.core.validation.Groups;
+import br.com.ivanfsilva.webfood.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
@@ -36,7 +37,8 @@ public class Restaurante {
 
 //    @DecimalMin("1")
     @NotNull
-    @PositiveOrZero(message = "{TaxaFrete.invalida}")
+//    @PositiveOrZero(message = "{TaxaFrete.invalida}")
+    @TaxaFrete
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
