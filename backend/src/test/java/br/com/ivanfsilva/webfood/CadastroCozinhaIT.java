f74296d48cfa3295bd30eebcbc08dc4e9ab8ab79
ivanfsilva <ivanfs27@gmail.com>
2021-09-01 22:20:59 -0300

    Implementing API Tests with REST Assured and Validating the HTTP Status Code

--- backend/src/test/java/br/com/ivanfsilva/webfood/CadastroCozinhaIT.java
+++ backend/src/test/java/br/com/ivanfsilva/webfood/CadastroCozinhaIT.java
@@ -6,21 +6,31 @@ import br.com.ivanfsilva.webfood.domain.model.Cozinha;
 import br.com.ivanfsilva.webfood.domain.service.CadastroCozinhaService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
 
 import javax.validation.ConstraintViolationException;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
 
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
+
+    @LocalServerPort
+    private int port;
 
     @Test
     public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
@@ -52,5 +62,19 @@ public class CadastroCozinhaIT {
     @Test(expected = CozinhaNaoEncontradaException.class)
     public void deveFalhar_QuandoExcluirCozinhaInexistente() {
         cadastroCozinha.excluir(100L);
+    }
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .statusCode(HttpStatus.OK.value());
     }
 }
