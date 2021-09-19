55246bb31833cf7ec9f817fad0c487c57da5fc33
ivanfsilva <ivanfs27@gmail.com>
2021-09-19 19:11:17 -0300

    Mapping to a target instance (not a type) with ModelMapper

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/assembler/RestauranteInputDisassembler.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/assembler/RestauranteInputDisassembler.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.webfood.api.assembler;
 
 import br.com.ivanfsilva.webfood.api.model.input.RestauranteInput;
+import br.com.ivanfsilva.webfood.domain.model.Cozinha;
 import br.com.ivanfsilva.webfood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,14 @@ public class RestauranteInputDisassembler {
 
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
         return modelMapper.map(restauranteInput, Restaurante.class);
+    }
+
+    public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+        // Para evitar org.hibernate.HibernateException: identifier of an instance of
+        // br.com.ivanfsilva.webfood.domain.model.Cozinha was altered from 1 to 2
+        restaurante.setCozinha(new Cozinha());
+
+        modelMapper.map(restauranteInput, restaurante);
     }
 
 }
