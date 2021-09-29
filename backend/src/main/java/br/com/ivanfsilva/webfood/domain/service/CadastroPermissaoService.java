e57a9dd0b226cedd4a275011d4e2b0c5b3c4aba5
ivanfsilva <ivanfs27@gmail.com>
2021-09-29 18:19:12 -0300

    implementing group membership endpoints with permissions

---
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/service/CadastroPermissaoService.java
@@ -0,0 +1,19 @@
+package br.com.ivanfsilva.webfood.domain.service;
+
+import br.com.ivanfsilva.webfood.domain.exception.PermissaoNaoEncontradaException;
+import br.com.ivanfsilva.webfood.domain.model.Permissao;
+import br.com.ivanfsilva.webfood.domain.repository.PermissaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroPermissaoService {
+
+    @Autowired
+    private PermissaoRepository permissaoRepository;
+
+    public Permissao buscarOuFalhar(Long permissaoId) {
+        return permissaoRepository.findById(permissaoId)
+                .orElseThrow(() -> new PermissaoNaoEncontradaException(permissaoId));
+    }
+}
