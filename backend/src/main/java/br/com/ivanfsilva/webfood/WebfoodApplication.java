08d5a94b8cb3e91bfebc534036bc73dc0ec61e0d
ivanfsilva <ivanfs27@gmail.com>
2021-09-28 22:18:51 -0300

    Implementing business rule to avoid users with duplicate emails

--- backend/src/main/java/br/com/ivanfsilva/webfood/WebfoodApplication.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/WebfoodApplication.java
@@ -1,11 +1,14 @@
 package br.com.ivanfsilva.webfood;
 
+import br.com.ivanfsilva.webfood.infrastructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class WebfoodApplication {
 
     public static void main(String[] args) {
