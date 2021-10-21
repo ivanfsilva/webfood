91c0f91e8ecb4056752011bd152afbac229909d2
ivanfsilva <ivanfs27@gmail.com>
2021-10-21 13:57:05 -0300

    Refactoring the Order Status Transition Rule Code

--- backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/StatusPedido.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/domain/model/StatusPedido.java
@@ -1,5 +1,8 @@
 package br.com.ivanfsilva.webfood.domain.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum StatusPedido {
 
     CRIADO("Criado"),
@@ -8,12 +11,18 @@ public enum StatusPedido {
     CANCELADO("Cancelado");
 
     private String descricao;
+    private List<StatusPedido> statusAnteriores;
 
-    StatusPedido(String descricao) {
+    StatusPedido(String descricao, StatusPedido... statusAnteriores) {
         this.descricao = descricao;
+        this.statusAnteriores = Arrays.asList(statusAnteriores);
     }
 
     public String getDescricao() {
-        return descricao;
+        return this.descricao;
+    }
+
+    public boolean naoPodeAlterarPara(StatusPedido novoStatus) {
+        return !novoStatus.statusAnteriores.contains(this);
     }
 }
