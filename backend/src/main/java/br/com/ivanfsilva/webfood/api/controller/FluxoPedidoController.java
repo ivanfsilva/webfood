2d81c43bb820324ed3d6138fe49d1c21e21eb2bc
ivanfsilva <ivanfs27@gmail.com>
2021-10-21 12:21:55 -0300

    Implementing order status transition endpoint

---
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/FluxoPedidoController.java
@@ -0,0 +1,32 @@
+package br.com.ivanfsilva.webfood.api.controller;
+
+import br.com.ivanfsilva.webfood.domain.service.FluxoPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping( value = "/pedidos/{pedidoId}")
+public class FluxoPedidoController {
+
+    @Autowired
+    private FluxoPedidoService fluxoPedido;
+
+    @PutMapping("/confirmacao")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void confimar( @PathVariable Long pedidoId ) {
+        fluxoPedido.confirmar(pedidoId);
+    }
+
+    @PutMapping("/cancelamento")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelar(@PathVariable Long pedidoId) {
+        fluxoPedido.cancelar(pedidoId);
+    }
+
+    @PutMapping("/entrega")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void entregar(@PathVariable Long pedidoId) {
+        fluxoPedido.entregar(pedidoId);
+    }
+}
