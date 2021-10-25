84d66731d6d04ef13234d36b8b027c12015b65a9
ivanfsilva <ivanfs27@gmail.com>
2021-10-25 13:52:30 -0300

    implementing query with aggregated daily sales data

--- backend/src/main/java/br/com/ivanfsilva/webfood/infrastructure/service/VendasQueryServiceImpl.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/infrastructure/service/VendasQueryServiceImpl.java
@@ -1,16 +1,40 @@
 package br.com.ivanfsilva.webfood.infrastructure.service;
 
 import br.com.ivanfsilva.webfood.domain.filter.VendaDiariaFilter;
+import br.com.ivanfsilva.webfood.domain.model.Pedido;
 import br.com.ivanfsilva.webfood.domain.model.dto.VendaDiaria;
 import br.com.ivanfsilva.webfood.domain.service.VendaQueryService;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
 import java.util.List;
 
+@Repository
 public class VendasQueryServiceImpl implements VendaQueryService {
+
+    @PersistenceContext
+    private EntityManager manager;
 
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-        return null;
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(VendaDiaria.class);
+        var root = query.from(Pedido.class);
+
+        var functionDateDataCriacao = builder.function(
+                "date", Date.class, root.get("dataCriacao"));
+
+        var selection = builder.construct(VendaDiaria.class,
+                functionDateDataCriacao,
+                builder.count(root.get("id")),
+                builder.sum(root.get("valorTotal")));
+
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        return manager.createQuery(query).getResultList();
     }
 
 }
