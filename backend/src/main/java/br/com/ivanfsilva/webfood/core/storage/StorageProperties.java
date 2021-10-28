141ba84f7c9153aa8390590537c030066cd77ea5
ivanfsilva <ivanfs27@gmail.com>
2021-10-27 23:53:23 -0300

    setting Amazon S3 client bean and configuring credentials

--- backend/src/main/java/br/com/ivanfsilva/webfood/core/storage/StorageProperties.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/core/storage/StorageProperties.java
@@ -1,7 +1,10 @@
 package br.com.ivanfsilva.webfood.core.storage;
 
+import com.amazonaws.regions.Regions;
+
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -30,7 +33,7 @@ public class StorageProperties {
         private String idChaveAcesso;
         private String chaveAcessoSecreta;
         private String bucket;
-        private String regiao;
+        private Regions regiao;
         private String diretorioFotos;
 
     }
--- backend/src/main/java/br/com/ivanfsilva/webfood/infrastructure/service/storage/S3FotoStorageService.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -3,10 +3,15 @@ package br.com.ivanfsilva.webfood.infrastructure.service.storage;
 import java.io.InputStream;
 
 import br.com.ivanfsilva.webfood.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
 
     @Override
     public InputStream recuperar(String nomeArquivo) {
