233afee5f4e4c9f454c7c84f4ecb98524e4967c8
ivanfsilva <ivanfs27@gmail.com>
2021-11-06 15:04:11 -0300

    describing media type of response on endpoints

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CidadeController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/CidadeController.java
@@ -17,6 +17,7 @@ import io.swagger.annotations.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,7 +25,7 @@ import java.util.List;
 
 @Api(tags = "Cidades")
 @RestController
-@RequestMapping(value = "/cidades")
+@RequestMapping(path = "/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CidadeController implements CidadeControllerOpenApi {
 
     @Autowired
--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/GrupoController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/GrupoController.java
@@ -14,13 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/grupos")
+@RequestMapping(path = "/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GrupoController implements GrupoControllerOpenApi {
 
     @Autowired
