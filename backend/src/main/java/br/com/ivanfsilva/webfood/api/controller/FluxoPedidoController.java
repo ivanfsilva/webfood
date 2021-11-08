fef2a2db82f334d0d7de75319b073fdc7c346aee
ivanfsilva <ivanfs27@gmail.com>
2021-11-08 18:10:05 -0300

    describing documentation for order flow endpoints

--- backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/FluxoPedidoController.java
+++ backend/src/main/java/br/com/ivanfsilva/webfood/api/controller/FluxoPedidoController.java
@@ -1,32 +1,35 @@
 package br.com.ivanfsilva.webfood.api.controller;
 
+import br.com.ivanfsilva.webfood.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import br.com.ivanfsilva.webfood.domain.service.FluxoPedidoService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping( value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = "/pedidos/{codigoPedido}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
 
     @Autowired
     private FluxoPedidoService fluxoPedido;
 
     @PutMapping("/confirmacao")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void confimar( @PathVariable String  codigoPedido ) {
+    public void confirmar(@PathVariable String codigoPedido) {
         fluxoPedido.confirmar(codigoPedido);
     }
 
     @PutMapping("/cancelamento")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancelar(@PathVariable String  codigoPedido) {
+    public void cancelar(@PathVariable String codigoPedido) {
         fluxoPedido.cancelar(codigoPedido);
     }
 
     @PutMapping("/entrega")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void entregar(@PathVariable String  codigoPedido) {
+    public void entregar(@PathVariable String codigoPedido) {
         fluxoPedido.entregar(codigoPedido);
     }
 }
